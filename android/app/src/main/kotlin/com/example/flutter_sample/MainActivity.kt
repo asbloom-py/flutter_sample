346a5240b7ac03ca8baebde479f44d780390bcae
monkeydaichan <daichi.yasuda@asigar.com>
2023-04-24 14:14:20 -0400

    Add

--- android/app/src/main/kotlin/com/example/flutter_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_sample/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sample_app
+package com.example.flutter_sample
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/resource/global/provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final navKeyProvider =
-    Provider<GlobalKey<NavigatorState>>((_) => GlobalKey<NavigatorState>());
